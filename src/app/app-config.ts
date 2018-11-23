d9d750fe9d61c0edfd5debe5876a524ddb84603a
Your Name <atharvab7@gmail.com>
2018-11-23 21:09:14 +0530

    Added static IP

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,9 +1,9 @@
 export class AppConfig {
 
   // endpoint for localhost.
-  public static API_ENDPOINT='http://localhost:8080';
+  //public static API_ENDPOINT='http://localhost:8080';
   public static AWS_S3_BUCKET = 'https://s3.ap-south-1.amazonaws.com/dev-logisticsindia-pan-data/';
 
   // aws host
-  //public static API_ENDPOINT = 'http://ec2-13-233-63-203.ap-south-1.compute.amazonaws.com:80';
+  public static API_ENDPOINT = 'http://ec2-52-66-45-201.ap-south-1.compute.amazonaws.com:8080';
 }
