cd2000ba36a45ea4dd6ca55dd570e4c9962e5e7e
Vikas Keshri <keshri.vikas@gmail.com>
2019-03-03 11:23:43 +0530

    corrected ec2 instance ip

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -5,5 +5,5 @@ export class AppConfig {
   public static AWS_S3_BUCKET = 'https://s3.ap-south-1.amazonaws.com/dev-logisticsindia-pan-data/';
 
   // aws host
-  public static API_ENDPOINT = 'http://ec2-3-18-42-17.us-east-2.compute.amazonaws.com:8080';
+  public static API_ENDPOINT = 'http://13.234.84.53:8080';
 }
